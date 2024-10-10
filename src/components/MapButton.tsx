8cff9076907291b2ba34b75191550922fe34ed36
sickandyoungbo <applejin.dev@gmail.com>
2024-10-10 17:12:11 +0900

    feat: add play store and app store link

--- src/components/MapButton.tsx
+++ src/components/MapButton.tsx
@@ -18,30 +18,76 @@ const MapButton = ({
     const encodedDestination = "%EC%9A%B0%EB%A6%AC%EB%93%A4%EA%B5%90%ED%9A%8C%20%ED%8C%90%EA%B5%90%EC%B1%84%ED%94%8C"
 
     if (name === 'TMAP') {
-      window.location.href = `tmap://search?name=${encodedDestination}`
+      const clickedAt = +new Date();
+    
+      if (isAndroid) {
+        window.location.href = `tmap://search?name=${encodedDestination}`;
+        
+        setTimeout(function() {
+          if (+new Date() - clickedAt < 2000) {
+            // TMAP 앱이 설치되지 않은 경우 구글 플레이 스토어로 이동
+            return window.location.href = 'https://play.google.com/store/apps/details?id=com.skt.tmap.ku';
+          }
+        }, 1500);
+    
+      } else if (isIos) {
+        window.location.href = `tmap://search?name=${encodedDestination}`;
+    
+        setTimeout(function() {
+          if (+new Date() - clickedAt < 2000) {
+            // TMAP 앱이 설치되지 않은 경우 앱스토어로 이동
+            return window.location.href = 'https://apps.apple.com/kr/app/t-map-%ED%8B%B0%EB%A7%B5-%EB%AA%A8%EB%B0%94%EC%9D%BC/id431589174';
+          }
+        }, 1500);
+      }
     }
-
+    
     if (name === '네이버지도') {
       const clickedAt = +new Date();
-
+    
       if (isAndroid) {
-        // Android 네이버 지도 앱 스킴
-        window.location.href = `intent://search?query${encodedDestination}=&appname=${appName}#Intent;scheme=nmap;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.nmap;end`;
-      } else if (isIos) {
-        // iOS 네이버 지도 앱 스킴
-        window.location.href = `nmap://search?query=${encodedDestination}&appname=${appName}`;
-
+        window.location.href = `intent://search?query=${encodedDestination}&appname=${appName}#Intent;scheme=nmap;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.nmap;end`;
+    
         setTimeout(function() {
-          // 만약 2초 이내에 앱이 열리지 않았다면, App Store로 이동
+          if (+new Date() - clickedAt < 2000) {
+            return window.location.href = 'https://play.google.com/store/apps/details?id=com.nhn.android.nmap';
+          }
+        }, 1500);
+    
+      } else if (isIos) {
+        window.location.href = `nmap://search?query=${encodedDestination}&appname=${appName}`;
+    
+        setTimeout(function() {
           if (+new Date() - clickedAt < 2000) {
             return window.location.href = 'http://itunes.apple.com/app/id311867728?mt=8';
           }
         }, 1500);
       }
     }
-
+    
     if (name === '카카오맵') {
-      window.location.href = `kakaomap://search?q=${encodedDestination}`;
+      const clickedAt = +new Date();
+    
+      if (isAndroid) {
+        window.location.href = `kakaomap://search?q=${encodedDestination}`;
+    
+        setTimeout(function() {
+          if (+new Date() - clickedAt < 2000) {
+            // 카카오맵 앱이 설치되지 않은 경우 구글 플레이 스토어로 이동
+            return window.location.href = 'https://play.google.com/store/apps/details?id=net.daum.android.map';
+          }
+        }, 1500);
+    
+      } else if (isIos) {
+        window.location.href = `kakaomap://search?q=${encodedDestination}`;
+    
+        setTimeout(function() {
+          if (+new Date() - clickedAt < 2000) {
+            // 카카오맵 앱이 설치되지 않은 경우 앱스토어로 이동
+            return window.location.href = 'https://apps.apple.com/kr/app/id304608425';
+          }
+        }, 1500);
+      }
     }
   }
 
