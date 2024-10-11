2c6bc6bec54ba35119d5b39ab9cd83125403db90
sickandyoungbo <applejin.dev@gmail.com>
2024-10-11 15:34:25 +0900

    이미지 순서 변경

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import ContentContainer from './components/ContentContainer';
 import SubTitle from './components/SubTitle';
 import WorshipItem from './components/WorshipItem';
 import MapButton from './components/MapButton';
+import CongratulatoryItem from './components/CongratulatoryItem';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
       <Main />
       <Header />
       <Section>
+          <div className='w-full h-5'></div>
           <Title>Wedding Day</Title>
           <ContentContainer>
             <p className='text-center text-[17px]'>
@@ -32,7 +34,7 @@ function App() {
               경기도 성남시 분당구 안양판교로 1219
             </p>
             <p className='pt-2 text-[17px] font-bold text-center'>우리들교회 판교채플 7층(탄설홀)</p>
-            <div className='px-[70px] pt-[30px]'>
+            <div className='px-[50px] pt-[30px]'>
               <img src={weddingDayPhotoUrl} />
             </div>
             <div className='pt-[30px] w-full dream-cottage'>
@@ -53,7 +55,7 @@ function App() {
         <Title>Greetings</Title>
         <ContentContainer>
           <div className='border-[1px] border-[#FADCE7] p-5 rounded-[13px]'>
-            <div className='text-base flex font-medium'>
+            <div className='text-base flex font-semibold'>
               <p className='pr-[14px]'>42</p>
               <p>
                 우리 두 사람이 여호와의 이름으로 맹세하여 이르기를여호와께서 영원히 나와 너 사이에 계시고...
@@ -75,11 +77,11 @@ function App() {
           </p>
           <div className='mt-[30px] border border-black rounded-[13px] py-[14px]'>
             <div className='flex text-base justify-center'>
-              <div className='flex flex-col pr-[10px] font-medium'>
+              <div className='flex flex-col pr-[10px] font-semiBold'>
                 <p>안성진</p>
                 <p>심재호</p>
               </div>
-              <div className='flex flex-col font-medium'>
+              <div className='flex flex-col font-semiBold'>
                 <p>이월순<span className='text-sm font-normal'>의</span></p>
                 <p>이경희<span className='text-sm font-normal'>의</span></p>
               </div>
@@ -107,7 +109,7 @@ function App() {
           <WorshipItem leftText='찬송' rightText='다같이'></WorshipItem>
           <WorshipItem leftText='기도' rightText='주례자'></WorshipItem>
           <WorshipItem leftText='봉독' rightText='주례자' hasMiddle={true} middleText='### NN:NN'></WorshipItem>
-          <div className='border-[1px] font-medium border-[#FADCE7] p-5 rounded-[13px] mt-5'>
+          <div className='border-[1px] font-semiBold border-[#FADCE7] p-5 rounded-[13px] mt-5'>
             <div className='flex'>
                 <p className='pr-[11px]'>22</p>
               <p>일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔</p>
@@ -131,10 +133,14 @@ function App() {
         </ContentContainer>
       </Section>
       <Section>
-        <Title>Photobook</Title>
-        <SubTitle>사진을 좌우로 넘겨보세요.</SubTitle>
-        <div className='pt-[30px]'>
-          <PhotobookSwiper />
+        <div className='flex flex-col items-center relative'>
+          <Title>Photobook</Title>
+          <SubTitle>사진을 좌우로 넘겨보세요.</SubTitle>
+          <div className='pt-[30px]'>
+            <PhotobookSwiper />
+          </div>
+          {/* 오시는 길 이동 포인트 */}
+          <div id='location' className='absolute bottom-[80.5px]'></div>
         </div>
       </Section>
       <Section>
@@ -142,7 +148,7 @@ function App() {
         <SubTitle>오시는 길을 안내해드립니다.</SubTitle>
         <ContentContainer>
           <Map latitude={37.38237} longitude={127.1014} />
-          <div className='mt-5 flex justify-between'>
+          <div className='pt-[30px] flex justify-between'>
             <MapButton iconUrl={tmapLogoUrl} name='TMAP' />
             <MapButton iconUrl={navermapLogoUrl} name='네이버지도' />
             <MapButton iconUrl={kakaomapLogoUrl} name='카카오맵' />
@@ -152,7 +158,20 @@ function App() {
       <Section>
         <Title>Congratulatory</Title>
         <SubTitle>마음을 전하실 곳</SubTitle>
-        <div className='pt-[30px]'></div>
+        <ContentContainer>
+          <CongratulatoryItem
+            who='groom'
+            bankName='카카오뱅크 안식'
+            accountNumber='1111-1111-1111-1111'
+          />
+          <div className='mt-[30px] mb-[60px]'>
+            <CongratulatoryItem
+              who='bride'
+              bankName='카카오뱅크 심영보'
+              accountNumber='1111-1111-1111-1111'
+            />
+          </div>
+        </ContentContainer>
       </Section>
     </div>
   );
